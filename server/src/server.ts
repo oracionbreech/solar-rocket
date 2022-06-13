3e3913cc622acc8ee2d720aac30f6893204e9f03
oracionbreech <oracionbre@gmail.com>
2022-06-14 01:40:15 +0800

    feat: get weather from weatherapi keyed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,41 @@
-import express, { Express, Request, Response } from "express";
+import express, {
+  Express,
+  Request,
+  Response,
+} from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { graphqlHTTP } from "express-graphql";
-import { readFile, access, mkdir, copyFile, writeFile } from "fs/promises";
+import {
+  readFile,
+  access,
+  mkdir,
+  copyFile,
+  writeFile,
+} from "fs/promises";
 import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { DateTimeResolver } from "graphql-scalars";
 import path from "path";
 import { GraphQLSchema } from "graphql";
 import { Mission } from "./types";
-import { CreateMission, GetMissionById, ListMissions } from "./queries";
+import {
+  CreateMission,
+  GetMissionById,
+  ListMissions,
+} from "./queries";
+import { weather } from "./services/weather";
 
-const DATA_DIR = path.join(__dirname, "../.data");
-const DATA_DIR_INIT = path.join(__dirname, "../data/init");
-const DATA_FILE_MISSIONS = "missions.json";
+const DATA_DIR = path.join(
+  __dirname,
+  "../.data"
+);
+const DATA_DIR_INIT = path.join(
+  __dirname,
+  "../data/init"
+);
+const DATA_FILE_MISSIONS =
+  "missions.json";
 const DATA_FILES = [DATA_FILE_MISSIONS];
 
 dotenv.config();
@@ -21,85 +44,186 @@ const app: Express = express();
 const port = process.env.PORT || 5000;
 
 let schema: GraphQLSchema;
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 app
-  .listen(port, () => console.log(`Listening on port ${port}`))
+  .listen(port, () =>
+    console.log(
+      `Listening on port ${port}`
+    )
+  )
   .on("error", (err) => {
     process.once("SIGUSR2", () => {
-      process.kill(process.pid, "SIGUSR2");
+      process.kill(
+        process.pid,
+        "SIGUSR2"
+      );
     });
     process.on("SIGINT", () => {
-      process.kill(process.pid, "SIGINT");
+      process.kill(
+        process.pid,
+        "SIGINT"
+      );
     });
   });
 
 // Wrapper function which initializes data if necessary
-const _g = async (callback?: Function) => {
+const _g = async (
+  callback?: Function
+) => {
   try {
     await access(DATA_DIR);
   } catch (err) {
-    console.warn(".data directory does not exist. Creating...");
-    await mkdir(DATA_DIR, { recursive: true });
+    console.warn(
+      ".data directory does not exist. Creating..."
+    );
+    await mkdir(DATA_DIR, {
+      recursive: true,
+    });
   }
 
   DATA_FILES.forEach(async (file) => {
     try {
-      await access(path.join(DATA_DIR, file));
+      await access(
+        path.join(DATA_DIR, file)
+      );
     } catch (err) {
-      console.warn(`${file} does not exist. Creating...`);
-      await copyFile(path.join(DATA_DIR_INIT, file), path.join(DATA_DIR, file));
+      console.warn(
+        `${file} does not exist. Creating...`
+      );
+      await copyFile(
+        path.join(DATA_DIR_INIT, file),
+        path.join(DATA_DIR, file)
+      );
     }
   });
 
   if (callback) return callback();
 };
 
-const loadMissions = async (): Promise<Mission[]> =>
+const loadMissions = async (): Promise<
+  Mission[]
+> =>
   await _g(async () => {
     return JSON.parse(
-      await readFile(path.join(DATA_DIR, DATA_FILE_MISSIONS), "utf8")
+      await readFile(
+        path.join(
+          DATA_DIR,
+          DATA_FILE_MISSIONS
+        ),
+        "utf8"
+      )
     );
   });
 
 const main = async () => {
   await _g();
 
-  schema = await loadSchema("./data/schema.graphql", {
-    loaders: [new GraphQLFileLoader()],
-    resolvers: {
-      DateTime: DateTimeResolver,
-      Query: {
-        async Missions(obj, args) {
-          const missions = await loadMissions();
-          return ListMissions(missions, args);
+  schema = await loadSchema(
+    "./data/schema.graphql",
+    {
+      loaders: [
+        new GraphQLFileLoader(),
+      ],
+      resolvers: {
+        DateTime: DateTimeResolver,
+        Query: {
+          async Missions(obj, args) {
+            const missions =
+              await loadMissions();
+            return ListMissions(
+              missions,
+              args
+            );
+          },
+          async Mission(obj, args) {
+            const missions =
+              await loadMissions();
+            return GetMissionById(
+              missions,
+              args.id
+            );
+          },
         },
-        async Mission(obj, args) {
-          const missions = await loadMissions();
-          return GetMissionById(missions, args.id);
+        Mutation: {
+          async createMission(
+            obj,
+            args
+          ) {
+            const missions =
+              await loadMissions();
+
+            const mission =
+              CreateMission(
+                args.mission
+              );
+            missions.push(mission);
+
+            await writeFile(
+              path.join(
+                DATA_DIR,
+                DATA_FILE_MISSIONS
+              ),
+              JSON.stringify(missions),
+              "utf8"
+            );
+            return mission;
+          },
         },
       },
-      Mutation: {
-        async createMission(obj, args) {
-          const missions = await loadMissions();
+    }
+  );
 
-          const mission = CreateMission(args.mission);
-          missions.push(mission);
-
-          await writeFile(
-            path.join(DATA_DIR, DATA_FILE_MISSIONS),
-            JSON.stringify(missions),
-            "utf8"
-          );
-          return mission;
-        },
-      },
-    },
-  });
-
-  app.get("/readme", 
-    async (req : Request, res: Response ) => {
-      const readme : String = await readFile(path.join(__dirname, "../../README.md"), "utf8")
+  app.get(
+    "/readme",
+    async (
+      req: Request,
+      res: Response
+    ) => {
+      const readme: String =
+        await readFile(
+          path.join(
+            __dirname,
+            "../../README.md"
+          ),
+          "utf8"
+        );
       res.send(readme);
+    }
+  );
+
+  const WEATHER_API_KEY =
+    "11974b5f7a384a2e80d170259221306";
+
+  app.get(
+    "/weather",
+    async (
+      req: Request,
+      res: Response
+    ) => {
+      try {
+        if (!req.query.city) {
+          return res.status(400).json({
+            message:
+              "Please provide city.",
+          });
+        }
+
+        const cityWeather =
+          await weather.get(
+            `https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${req.query.city}&days=5&aqi=no&alerts=no`
+          );
+
+        return res.send(
+          cityWeather.data
+        );
+      } catch (error) {
+        return res.send(error);
+      }
     }
   );
 
@@ -107,7 +231,9 @@ const main = async () => {
     "/graphql",
     graphqlHTTP({
       schema,
-      graphiql: { headerEditorEnabled: false },
+      graphiql: {
+        headerEditorEnabled: false,
+      },
     })
   );
 };
